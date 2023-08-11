b39cff75db8d67d39bedb015aa4f53b171951cca
Зубов Олександрович <a.zubov.tv11@kpi.ua>
2023-08-11 10:47:58 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,6 +4,9 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from blog.models import Post
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 user_model = get_user_model()
@@ -12,7 +15,8 @@ register = template.Library()
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
-    return {"title": "Recent Posts", "posts": posts}
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
+    return {"title": "Recent Posts", "post": posts}
 
 @register.filter
 def author_details(author, current_user):
