d4564945012b0b48147743c1052455c7e88c7010
Зубов Олександрович <a.zubov.tv11@kpi.ua>
2023-08-12 11:51:51 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -42,7 +42,11 @@ class Dev(Configuration):
     CRISPY_TEMPLATE_PACK = "bootstrap5"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
-
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
     # Application definition
 
     INSTALLED_APPS = [
@@ -51,12 +55,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         "debug_toolbar",
         'blango_auth',
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
         
     ]
 
