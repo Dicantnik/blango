96e7287c22f2116a3b426f991ce048b79ffff742
Зубов Олександрович <a.zubov.tv11@kpi.ua>
2023-08-20 11:10:51 +0000

    Finish nested relationships

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from blog.api.permissions import AuthorModifyOrReadOnly
-from blog.api.serializers import PostSerializer, UserSerializer
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blog.models import Post
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blango_auth.models import User
@@ -20,3 +20,9 @@ class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
